8fb0420a147c173e4c051c5c0751855625676a5e
priyasangeeth <33655041+priyasangeeth@users.noreply.github.com>
2018-12-03 01:23:01 +0000

    added log out function

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-describe('Go to Tray.io', function() {
+describe('Access Tray.io, login, create workflow, delete the created workflow and log out from the application', function() {
  it('Login to Tray.io', function() {
 		browser.ignoreSynchronization = true;
 		browser.get('https://app.tray.io/');
@@ -12,23 +12,33 @@ describe('Go to Tray.io', function() {
 	});
 	
  it('create workflow', function() {
-		element(by.css("a[href*='create']")).click();
+		element(by.css("a[href*='create']")).click(); //click on create workflow
 		browser.sleep(3000);
-		element(by.css("input[name='name']")).sendKeys('demotest1');
+		element(by.css("input[name='name']")).sendKeys('priyaWorkflow');
 		element(by.css("div[title='Save']")).click();
-		browser.sleep(3000);
-		element(by.css(".Connectors-grid-element___207RbN:nth-of-type(4)")).click();
-		browser.sleep(3000);
+		browser.sleep(8000);
+		element(by.css(".Connectors-grid-element___207RbN:nth-of-type(4)")).click(); //selecting random connector from the grid
+		browser.sleep(5000);
 		element(by.css("a[title='Close']")).click();
-		browser.sleep(3000);
+		browser.sleep(5000);
 	});
 	 
- it('delete workflow from dashboard', function() {
-		browser.actions().mouseMove(element(by.css("div[qa='workflow-options-menu']"))).perform();
-		element(by.css("div[qa='workflow-options-menu'] ul>li:nth-of-type(5) div")).click();
-		browser.sleep(3000);
+ it('delete the last created workflow from dashboard', function() {
+		element(by.css("input[qa='workflows-list-search']")).sendKeys('priyaWorkflow'); //searching for the lastly created workflow
+		browser.sleep(1000);
+		expect((element(by.css("h2[qa='workflow-title']"))).getText()).toEqual('priyaWorkflow'); //assertion on the lastly created workflow name
+		browser.actions().mouseMove(element(by.css("div[qa='workflow-options-menu']"))).perform();  // mouse over on the options
+		element(by.css("div[qa='workflow-options-menu'] ul>li:nth-of-type(5) div")).click(); //click on delete
+		browser.sleep(5000);
 		element(by.css("div[title='Yes']")).click();
-		browser.sleep(3000);
+		browser.sleep(5000);
+		element(by.css("input[qa='workflows-list-search']")).clear();
 	});
  
+ it('log out from Tray.io', function() {
+		element(by.css("#avatar")).click();
+		element(by.css("a[href*='/logout']")).click();
+		browser.sleep(3000);
+	});
+	
 });

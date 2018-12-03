df83f35d0e85ed0ded477e2c8c6940b37c53146b
priyasangeeth <33655041+priyasangeeth@users.noreply.github.com>
2018-12-03 01:21:51 +0000

    updated the file

--- conf.js
+++ conf.js
@@ -2,13 +2,12 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   capabilities: {
 	  'browserName': 'chrome'
-  },
-  
+  },  
   framework:'jasmine',
-  
   specs: ['test.js'],
-  
+   
   jasmineNodeOpts: {
 	  defaultTimeoutInterval: 100000
   }
+
 };
